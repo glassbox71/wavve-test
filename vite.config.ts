839c7a5b1190e6a010e8653ac0c4dcc07202cb45
glassbox71 <glassbox71@naver.com>
2025-12-23 22:14:12 +0900

    아이콘 경로수정

--- vite.config.ts
+++ vite.config.ts
@@ -29,12 +29,12 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: '/m-icon.png',
+            src: '/m-icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/m-icon.png',
+            src: '/m-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
