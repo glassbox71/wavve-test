001ea048f276cb490c7763be1ba88c03e67ae2a9
glassbox71 <glassbox71@naver.com>
2025-12-23 22:34:12 +0900

    33

--- vite.config.ts
+++ vite.config.ts
@@ -11,38 +11,37 @@
 //     }),
 //   ],
 // })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-   VitePWA({
-  registerType: 'autoUpdate',
-  manifest: {
-    name: 'Wave-app',
-    short_name: 'Wave',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
-      {
-        src: '/m-icon-192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'any maskable'
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My React App",
+        short_name: "Wavve app",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/m-icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/m-icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable", //
+          },
+        ],
       },
-      {
-        src: '/m-icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable'
-      }
-    ],
-  },
-});
-
+    }),
   ],
 });
