7216b9f0c8f4b747671c02b4b03904a5fbfde386
glassbox71 <glassbox71@naver.com>
2025-12-24 10:13:05 +0900

    아이콘 여백조절

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,13 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/Wavve_192.svg",
+            src: "/Wavve_192x192.svg",
             sizes: "192x192",
             type: "image/svg+xml",
             purpose: "any maskable",
           },
           {
-            src: "/Wavve_512.png",
+            src: "/Wavve_512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable", //
