37e87133f2615c6c234570e7df34724b94418c1d
glassbox71 <glassbox71@naver.com>
2025-12-24 10:03:30 +0900

    아이콘 변경

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,13 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/m-icon-192.svg",
+            src: "/Wavve_192.svg",
             sizes: "192x192",
             type: "image/svg+xml",
             purpose: "any maskable",
           },
           {
-            src: "/m-icon-512.png",
+            src: "/Wavve_512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable", //
